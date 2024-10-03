480d5a8c967f97330281e601060a98e50c633d39
sirk71171 <175373365+sirk71171@users.noreply.github.com>
2024-10-03 03:10:44 -0600

    allowed for the creation of Book objects that are capable of being added to a library (an array). The Book objects, so far, contain information about a book.

---
+++ script.js
@@ -0,0 +1,49 @@
+const myLibrary = [];
+
+function Book(author, title, pages, read, notRead) {
+    this.author = author;
+    this.title = title;
+    this.pages = parseInt(pages, 10);
+    this.read = readON();
+}
+
+const bookSubmission = document.querySelector("submit");
+const authorName = document.getElementById("author");
+const titleName = document.getElementById("title");
+const pagesNumber = document.getElementById("number-of-pages");
+const readOrNot = document.getElementsByName("read");
+
+const author = authorName.value;
+const title = titleName.value;
+const pages = pagesNumber.value;
+
+function readON () {
+    for (i = 0; i < 2; i++) {
+        if (readOrNot[i].checked) {
+            return readOrNot[i].value;
+        }
+            
+        
+    }
+}
+
+const form = document.getElementById("my-form");
+function handleForm (event) {
+    event.preventDefault();
+    const author = authorName.value;
+    const title = titleName.value;
+    const pages = pagesNumber.value;
+    const readNotRead = readON();
+
+    addBookToLibrary(author, title, pages, readNotRead);
+    
+}
+form.addEventListener("submit", handleForm)
+
+function addBookToLibrary(author, title, pages, read) {
+    const bookEntry = new Book(author, title, pages, read);
+    myLibrary.push(bookEntry);
+    
+}
+
+console.log(myLibrary);
